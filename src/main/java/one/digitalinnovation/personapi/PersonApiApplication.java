0bbe4bff961aba3c3d0c78d979ef57b85bc1c81d
amorim00 <ricardoamorim00@gmail.com>
2021-09-02 08:24:52 -0300

    Atualizando dependencias

--- src/main/java/one/digitalinnovation/personapi/PersonApiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonApiApplication.java
@@ -1,6 +1,5 @@
 package one.digitalinnovation.personapi;
 
-import one.digitalinnovation.personapi.controller.PersonController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
