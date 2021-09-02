0ccc6f376d659cdf112beefe988fd6e352708f1c
amorim00 <ricardoamorim00@gmail.com>
2021-09-01 21:12:45 -0300

    Atualizando projeto

--- src/main/java/one/digitalinnovation/personapi/PersonApiApplication.java
+++ src/main/java/one/digitalinnovation/personapi/PersonApiApplication.java
@@ -8,10 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PersonApiApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(PersonApiApplication.class, args);
-        PersonController personController = new PersonController();
-        personController.getBook();
     }
 
 }
